b71ff70d45f1a59a90bc6f4e5c7da907e1777637
sanaeas <sanaassim1@gmail.com>
2023-11-03 13:06:10 +0100

    dynamic amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityID = $(this).data("id");
+        const amenityName = $(this).data("name");
+
+        if ($(this).is(":checked")) {
+            selectedAmenities[amenityID] = amenityName;
+        } else {
+            delete selectedAmenities[amenityID];
+        }
+
+        const amenitiesList = Object.values(selectedAmenities).join(", ");
+        $(".filters .amenities h4").text(amenitiesList);
+    });
+});
